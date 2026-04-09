fb013a225f3b5b755dd952637faff7439b6228b7
HafizAhsan187 <hahsan1871@gmail.com>
2026-04-09 10:56:13 +0500

    save age

--- VolunteerPortal-API/UMISModuleApi/dto/request/mobileUserCreation.cs
+++ VolunteerPortal-API/UMISModuleApi/dto/request/mobileUserCreation.cs
@@ -10,20 +10,14 @@ namespace UMISModuleApi.Entities
         public int userID { get; set; }
         public string userName { get; set; }
         public string email { get; set; }
-        //public string password { get; set; }
         public string cnic { get; set; }
         public int genderID { get; set; }
         public int cityID { get; set; }
         public int countryID { get; set; }
-        //public string fatherName { get; set; }
+        public int age { get; set; }
         public int physicallyFit { get; set; }
         public string contactNo { get; set; }
         public string address { get; set; }
-        // public string eResume { get; set; }
-        // public string eResumePath { get; set; }
-        // public string eResumeExt { get; set; }
-        // public string eDoc { get; set; }
-        // public string eDocPath { get; set; }
         public string json { get; set; }
         public int roleID { get; set; }
         public int userTypeID { get; set; }
